41204a4b6cdff93317a162b646a4157aceec3a62
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-02-03 01:56:18 +0900

    GUIをちょっとだけつけました

---
+++ Rivals.java
@@ -0,0 +1,116 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Rivals extends JPanel implements ActionListener{
+    JPanel p1 = new JPanel();
+    JPanel p2 = new JPanel();
+    JLabel l,la,lb;
+    JLabel[] l1 = new JLabel[10];
+    JLabel[] l2 = new JLabel[10];
+    JButton b1,b2,b3;
+
+    public Rivals(){
+      int[] a = new int[10];
+      int[] b = new int[10];
+      ImageIcon icon1 = new ImageIcon("rock.jpg");
+      ImageIcon icon2 = new ImageIcon("Scissors.jpg");
+      ImageIcon icon3 = new ImageIcon("Paper.jpg");
+
+      for(int j = 0; j < 2; j++){
+        if(j == 0){
+          for(int i = 0; i < 10; i++){
+            Random rnd = new Random();
+            int ran = rnd.nextInt(3) + 1;
+          if(ran == 1)
+            a[i] = 1;
+          else if(ran == 2)
+            a[i] = 2;
+          else
+            a[i] = 3;
+          }
+        }
+        else{
+          for(int i = 0; i < 10; i++){
+            Random rnd = new Random();
+            int ran = rnd.nextInt(3) + 1;
+          if(ran == 1)
+            b[i] = 1;
+          else if(ran == 2)
+            b[i] = 2;
+          else
+            b[i] = 3;
+          }
+        }
+      }
+      la = new JLabel("先攻のカード");
+      lb = new JLabel("後攻のカード");
+      p1.add(la);
+      p2.add(lb);
+	for(int i = 0; i < 10; i++){
+    if(a[i] == 1){
+	    l1[i] = new JLabel(icon1);
+    }else if(a[i] == 2){
+      l1[i] = new JLabel(icon2);
+    }else if(a[i] == 3){
+      l1[i] = new JLabel(icon3);
+    }
+
+    if(b[i] == 1){
+      l2[i] = new JLabel(icon1);
+    }else if(b[i] == 2){
+      l2[i] = new JLabel(icon2);
+    }else if(b[i] == 3){
+      l2[i] = new JLabel(icon3);
+    }
+	    p1.add(l1[i]);
+	    p2.add(l2[i]);
+	}
+	l = new JLabel("じゃんけんライバルズ");
+	b1 = new JButton("ゲームスタート");
+	b2 = new JButton("Next");
+	b3 = new JButton("Start");
+
+  b1.addActionListener(this);
+  b2.addActionListener(this);
+  b3.addActionListener(this);
+
+	p1.add(b2);
+	p2.add(b3);
+
+	setLayout(new BorderLayout());
+	add(l,BorderLayout.NORTH);
+	add(b1,BorderLayout.SOUTH);
+	add(p1,BorderLayout.WEST);
+  add(p2,BorderLayout.EAST);
+	p1.setVisible(false);
+  p2.setVisible(false);
+	b1.setVisible(true);
+}
+
+public void actionPerformed(ActionEvent e){
+  Object obj = e.getSource();
+  if(obj == b1){
+    b1.setVisible(false);
+    p1.setVisible(true);
+  }
+  else if(obj == b2){
+    p1.setVisible(false);
+    p2.setVisible(true);
+  }
+  else if(obj == b3){
+    p2.setVisible(false);
+  }
+}
+
+public static void main(String[] args){
+    JFrame frame = new JFrame("Rivals");
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Rivals r = new Rivals();
+    frame.add(r,BorderLayout.CENTER);
+    frame.pack();
+    frame.setVisible(true);
+}
+}
--- user_Main.java
+++ user_Main.java
@@ -1,20 +1,113 @@
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-class user_Main{
+
+class user_Main extends JFrame implements ActionListener{
+
+  JPanel panel;
+  CardLayout layout;
 
   public static int a_hands[] = {1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
   public static int b_hands[] = {1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
-  public static int a_field[] = {0, 0, 0, 0, 0, 0};
-  public static int b_field[] = {1, 2, 3, 1, 2, 3};
+  public static int a_field[][] = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+  public static int b_field[][] = {{1, 2, 3, 1, 2, 3}, {0, 0, 0, 0, 0, 0}};
 
   public static int a_hp = 6;
   public static int b_hp = 6;
 
   public static void main(String args[]){
+    user_Main frame = new user_Main();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(200, 200, 600, 400);
+    frame.setTitle("タイトル");
+    frame.setVisible(true);
     while(true){
       printField();
       doneCommand();
     }
+  }
+
+  user_Main(){
+    
+    /* 対戦ページ */
+    JPanel pA = new JPanel();
+    pA.setLayout(new GridLayout(3, 2));
+    pA.add(new Button("one"));    
+    pA.add(new Button("two"));    
+    pA.add(new Button("three"));    
+    pA.add(new Button("four"));    
+    pA.add(new Button("five"));    
+    pA.add(new Button("six"));    
+
+    JPanel pB = new JPanel();
+    pB.setLayout(new GridLayout(3, 2));
+    pB.add(new Button("one"));    
+    pB.add(new Button("two"));    
+    pB.add(new Button("three"));    
+    pB.add(new Button("four"));    
+    pB.add(new Button("five"));    
+    pB.add(new Button("six"));    
+
+    JPanel hands = new JPanel();
+    hands.setLayout(new GridLayout(0, 10));
+    hands.add(new Button("a"));
+    hands.add(new Button("b"));
+    hands.add(new Button("c"));
+    hands.add(new Button("d"));
+    hands.add(new Button("e"));
+    hands.add(new Button("f"));
+    hands.add(new Button("g"));
+    hands.add(new Button("h"));
+    hands.add(new Button("i"));
+    hands.add(new Button("j"));
+
+    JPanel card1 = new JPanel();
+    card1.setLayout(new GridLayout(0, 4));
+    card1.add(new Button("PlayerA"), 0);
+    card1.add(pA, 1);
+    card1.add(pB, 2);
+    card1.add(new Button("PlayerB"), 3);
+
+    JPanel all = new JPanel();
+    all.setLayout(new BoxLayout(all, BoxLayout.Y_AXIS));
+    all.add(new JLabel("天の声"));
+    all.add(card1);
+    all.add(hands);
+
+    /* View2 */
+    JPanel card2 = new JPanel();
+    card2.add(new JLabel("View2"));
+ 
+    /* CardLayout準備 */
+    this.panel = new JPanel();
+    this.layout = new CardLayout();//CardLayoutの作成
+    this.panel.setLayout(this.layout);
+    /* panelにViewを追加 */
+    this.panel.add(all, "View1");
+    this.panel.add(card2, "View2");
+    
+    /* カード移動用ボタン */
+    JButton button1 = new JButton("View1");
+    button1.addActionListener(this);
+    button1.setActionCommand("View1");
+
+    JButton button2 = new JButton("View2");
+    button2.addActionListener(this);
+    button2.setActionCommand("View2");
+
+    JPanel btnPanel = new JPanel();
+    btnPanel.add(button1);
+    btnPanel.add(button2);
+
+    getContentPane().add(this.panel, BorderLayout.CENTER);
+    getContentPane().add(btnPanel, BorderLayout.PAGE_END);
+  }
+  public void actionPerformed(ActionEvent e) {
+    // TODO 自動生成されたメソッド・スタブ
+    String cmd = e.getActionCommand();
+    layout.show(this.panel,cmd);
   }
 
   public static void doneCommand(){
@@ -36,11 +129,11 @@ class user_Main{
 
   public static void printField(){
     System.out.println("   ========  ========");
-    System.out.println("   = " + a_field[0] + "  " + a_field[3] + " =  = " + b_field[3] + "  " + b_field[0] + " =");
+    System.out.println("   = " + a_field[0][0] + "  " + a_field[0][3] + " =  = " + b_field[0][3] + "  " + b_field[0][0] + " =");
     System.out.println("   =      =  =      =");
-    System.out.println(" " + a_hp + " = " + a_field[1] + "  " + a_field[4] + " =  = " + b_field[4] + "  " + b_field[1] + " = " + b_hp);
+    System.out.println(" " + a_hp + " = " + a_field[0][1] + "  " + a_field[0][4] + " =  = " + b_field[0][4] + "  " + b_field[0][1] + " = " + b_hp);
     System.out.println("   =      =  =      =");
-    System.out.println("   = " + a_field[2] + "  " + a_field[5] + " =  = " + b_field[5] + "  " + b_field[2] + " =");
+    System.out.println("   = " + a_field[0][2] + "  " + a_field[0][5] + " =  = " + b_field[0][5] + "  " + b_field[0][2] + " =");
     System.out.println("   ========  ========");
     for(int i = 0; i < 10; i++){
       System.out.print(a_hands[i] + " ");
@@ -58,7 +151,7 @@ class user_Main{
     Scanner field = new Scanner(System.in);
     String Tfield = field.next();
     
-    a_field[Integer.parseInt(Tfield)] = a_hands[Integer.parseInt(Thand)];
+    a_field[0][Integer.parseInt(Tfield)] = a_hands[Integer.parseInt(Thand)];
     a_hands[Integer.parseInt(Thand)] = 0;
   }
 
@@ -75,9 +168,9 @@ class user_Main{
     int att = Integer.parseInt(attack);
     int rec = Integer.parseInt(receive);
 
-    if((a_field[att] == 1 && b_field[rec] == 2) || (a_field[att] == 2 && b_field[rec] == 3) || (a_field[att] == 3 && b_field[rec] == 1)){
+    if((a_field[0][att] == 1 && b_field[0][rec] == 2) || (a_field[0][att] == 2 && b_field[0][rec] == 3) || (a_field[0][att] == 3 && b_field[0][rec] == 1)){
       System.out.println(" > 攻撃成功");
-      b_field[rec] = 0;
+      b_field[0][rec] = 0;
     }else{
       System.out.println(" > 攻撃失敗");
     }
@@ -85,7 +178,7 @@ class user_Main{
 
   public static void attackP(){
     //System.out.println("相手のプレイヤーを攻撃します.");
-    System.out.print;lk(" > どのカードで攻撃しますか?: ");
+    System.out.println(" > どのカードで攻撃しますか?: ");
     Scanner challenger = new Scanner(System.in);
     String challenge = challenger.next();
     b_hp = b_hp - 1;
