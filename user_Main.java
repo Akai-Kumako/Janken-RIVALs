b9bc850dbaf4d25ee355bac42414bb5e86c42224
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-02-03 11:47:33 +0900

    よくわからん

--- user_Main.java
+++ user_Main.java
@@ -12,7 +12,7 @@ class user_Main extends JFrame implements ActionListener{
   public static int a_hands[] = {1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
   public static int b_hands[] = {1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
   public static int a_field[][] = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
-  public static int b_field[][] = {{1, 2, 3, 1, 2, 3}, {0, 0, 0, 0, 0, 0}};
+  public static int b_field[][] = {{1, 2, 3, 1, 2, 3}, {0, 0, 0, 0, 0, 9}};
 
   public static int a_hp = 6;
   public static int b_hp = 6;
@@ -20,7 +20,7 @@ class user_Main extends JFrame implements ActionListener{
   public static void main(String args[]){
     user_Main frame = new user_Main();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setBounds(200, 200, 600, 400);
+    frame.setBounds(200, 200, 800, 400);
     frame.setTitle("タイトル");
     frame.setVisible(true);
     while(true){
@@ -30,63 +30,99 @@ class user_Main extends JFrame implements ActionListener{
   }
 
   user_Main(){
+
+    JLabel tennokoe = new JLabel("天の声");
+
+    Button playerA = new Button("PlayerA");
+    Button playerB = new Button("PlayerB");
+   
+    Button a1 = new Button("one");
+    Button a2 = new Button("two");
+    Button a3 = new Button("three");
+    Button a4 = new Button("four");
+    Button a5 = new Button("five");
+    Button a6 = new Button("six");
+
+    Button b1 = new Button("one");
+    Button b2 = new Button("one");
+    Button b3 = new Button("one");
+    Button b4 = new Button("one");
+    Button b5 = new Button("one");
+    Button b6 = new Button("one");
+
+    Button aA = new Button("one");
+    Button aB = new Button("one");
+    Button aC = new Button("one");
+    Button aD = new Button("one");
+    Button aE = new Button("one");
+    Button aF = new Button("one");
+    Button aG = new Button("one");
+    Button aH = new Button("one");
+    Button aI = new Button("one");
+    Button aJ = new Button("one");
+    
+    Button bA = new Button("one");
+    Button bB = new Button("one");
+    Button bC = new Button("one");
+    Button bD = new Button("one");
+    Button bE = new Button("one");
+    Button bF = new Button("one");
+    Button bG = new Button("one");
+    Button bH = new Button("one");
+    Button bI = new Button("one");
+    Button bJ = new Button("one");
     
     /* 対戦ページ */
     JPanel pA = new JPanel();
     pA.setLayout(new GridLayout(3, 2));
-    pA.add(new Button("one"));    
-    pA.add(new Button("two"));    
-    pA.add(new Button("three"));    
-    pA.add(new Button("four"));    
-    pA.add(new Button("five"));    
-    pA.add(new Button("six"));    
+    pA.add(a1);    
+    pA.add(a2);    
+    pA.add(a3);    
+    pA.add(a4);    
+    pA.add(a5);    
+    pA.add(a6);    
 
     JPanel pB = new JPanel();
     pB.setLayout(new GridLayout(3, 2));
-    pB.add(new Button("one"));    
-    pB.add(new Button("two"));    
-    pB.add(new Button("three"));    
-    pB.add(new Button("four"));    
-    pB.add(new Button("five"));    
-    pB.add(new Button("six"));    
+    pB.add(b1);    
+    pB.add(b2);    
+    pB.add(b3);    
+    pB.add(b4);    
+    pB.add(b5);    
+    pB.add(b6);    
 
     JPanel hands = new JPanel();
     hands.setLayout(new GridLayout(0, 10));
-    hands.add(new Button("a"));
-    hands.add(new Button("b"));
-    hands.add(new Button("c"));
-    hands.add(new Button("d"));
-    hands.add(new Button("e"));
-    hands.add(new Button("f"));
-    hands.add(new Button("g"));
-    hands.add(new Button("h"));
-    hands.add(new Button("i"));
-    hands.add(new Button("j"));
+    hands.add(aA);
+    hands.add(aB);
+    hands.add(aC);
+    hands.add(aD);
+    hands.add(aE);
+    hands.add(aF);
+    hands.add(aG);
+    hands.add(aH);
+    hands.add(aI);
+    hands.add(aJ);
 
     JPanel card1 = new JPanel();
     card1.setLayout(new GridLayout(0, 4));
-    card1.add(new Button("PlayerA"), 0);
+    card1.add(playerA, 0);
     card1.add(pA, 1);
     card1.add(pB, 2);
-    card1.add(new Button("PlayerB"), 3);
+    card1.add(playerB, 3);
 
     JPanel all = new JPanel();
     all.setLayout(new BoxLayout(all, BoxLayout.Y_AXIS));
-    all.add(new JLabel("天の声"));
+    all.add(tennokoe);
     all.add(card1);
     all.add(hands);
 
-    /* View2 */
-    JPanel card2 = new JPanel();
-    card2.add(new JLabel("View2"));
- 
     /* CardLayout準備 */
     this.panel = new JPanel();
     this.layout = new CardLayout();//CardLayoutの作成
     this.panel.setLayout(this.layout);
     /* panelにViewを追加 */
     this.panel.add(all, "View1");
-    this.panel.add(card2, "View2");
     
     /* カード移動用ボタン */
     JButton button1 = new JButton("View1");
